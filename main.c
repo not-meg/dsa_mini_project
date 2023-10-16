d91e8907fefa137922edf39ef6fb6edfdb191409
not-meg <116863014+not-meg@users.noreply.github.com>
2023-10-16 07:29:17 +0000

    updated main.c

--- main.c
+++ main.c
@@ -4,44 +4,50 @@
 
 struct flight_node
 {
-	char source[30];
-	char destination[30];
-	char ans;
-	int distance;
-	int duration;
-	int cost;
+    char source[30];
+    char destination[30];
+    int distance;
+    int duration;
+    int cost;
 };
 
 int main()
 {
-	char source[50], destination[50];
-	int no_of_stops, sort_by, i;
-	char places ["Natal (RN)", "Recife (PE)", "Rio de Janeiro (RJ)", "Campo Grande (MS)", "Salvador (BH)", "Aracaju (SE)", "Sao Paulo (SP)", "Brasilia (DF)", "Florianopolis (SC)"];
+    char source[50], destination[50], ans;
+    int no_of_stops, sort_by, i, sort_by;
+    char *places[] = {"Natal (RN)", "Recife (PE)", "Rio de Janeiro (RJ)", "Campo Grande (MS)", "Salvador (BH)", "Aracaju (SE)", "Sao Paulo (SP)", "Brasilia (DF)", "Florianopolis (SC)"};
 
-	printf("Places:\n");
+    printf("Places:\n");
 
-	for(i=0; i<9; i++) {
-		printf("places[i]\n");
-	}
+    for(i=0; i<9; i++) {
+        printf("%s\n", places[i]);
+    }
 
-	printf("Enter the source location: ");
-	scanf("%s", source);
+    printf("Enter the source location: ");
+    scanf("%s", source);
 
-	printf("Enter destination: ");
-	scanf("%s", destination);
+    printf("Enter destination: ");
+    scanf("%s", destination);
 
-	printf("Do you want to add any stops? (Y/N) ");
-	scanf("%c", &ans);
+    while(getchar() != '\n'); // Clear input buffer
 
-	if(ans == 'Y' || ans == 'y') {
-		printf("Enter the number of stops: ");
-		scanf("%d", no_of_stops);
-		char stops[no_of_stops];
+    printf("Do you want to add any stops? (Y/N) ");
+    scanf("%c", &ans);
 
-		for(i=0; i<no_of_stops; i++) {
-			printf("Enter the stop: ");
-			scanf("%s", &stops[i]);
-		}
-	}
+    if(ans == 'Y' || ans == 'y') {
+        printf("Enter the number of stops: ");
+        scanf("%d", &no_of_stops);
+        char stops[no_of_stops][50];
+
+        for(i=0; i<no_of_stops; i++) {
+            printf("Enter the stop: ");
+            scanf("%s", stops[i]);
+        }
+    }
+
+	printf("How do you want to find a route?\nBased on:\n1. Shortest Duration\nLowest Price\n");
+	scanf("%d", &sort_by);
+
 	
+    return 0;
 }
