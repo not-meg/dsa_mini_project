be4eaceb4d988765b609798ecfff00ea89864a8f
not-meg <116863014+not-meg@users.noreply.github.com>
2023-10-16 09:28:40 +0530

    Create main.c

---
+++ main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+struct flight_node
+{
+  char source[30];
+  char destination[30];
+	int distance;
+	int duration;
+	int cost;
+};
+
+int main()
+{
+	
+}
