524a53e669ec2e4e2505ae7721ca0cb39c5ba6a4
not-meg <116863014+not-meg@users.noreply.github.com>
2023-10-24 16:19:02 +0530

    Update project_header.h

--- project_header.h
+++ project_header.h
@@ -11,19 +11,28 @@ struct FlightInfo {
     int distance;
 };
 
+struct adjacency_matrix_data
+{
+    float price;
+    int duration;
+};
+
 struct allpaths
 {
     int path[MAX_ROWS];
     int duration;
     float cost;
+    int path_len;
 };
 
-void display_adj(int adj[MAX_ROWS][MAX_ROWS][2], int rows);
+void display_adj(struct adjacency_matrix_data adj[MAX_ROWS][MAX_ROWS], int rows);
 void display_places(char places[MAX_ROWS][MAX_LEN], int rows);
 int get_flight_data(struct FlightInfo *data, int maxFlightCount);
 int find_all_unique_places(struct FlightInfo *data, char places[MAX_ROWS][MAX_LEN], int no_of_rows);
-void create_adjacency_matrix(struct FlightInfo *data, int adj[MAX_ROWS][MAX_ROWS][2], int no_of_input_rows,
+void create_adjacency_matrix(struct FlightInfo *data, struct adjacency_matrix_data adj[MAX_ROWS][MAX_ROWS], int no_of_input_rows,
                              char places[MAX_ROWS][MAX_LEN], int unique_places_rows);
 int find_path(char start[MAX_LEN], char end[MAX_LEN], int unique_places_rows,
-              struct allpaths **all_paths_src_dst, int adj[MAX_ROWS][MAX_ROWS][2],
+              struct allpaths **all_paths_src_dst, struct adjacency_matrix_data adj[MAX_ROWS][MAX_ROWS],
               char places[MAX_ROWS][MAX_LEN]);
+void find_total_cost_duration(struct allpaths *all_paths_src_dst, int allpaths_row,
+                              struct adjacency_matrix_data adj[MAX_ROWS][MAX_ROWS]);
