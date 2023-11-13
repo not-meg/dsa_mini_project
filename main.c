27a5f6833dd1da36f2844cba1dba220881105339
MeeraReji <117140686+MeeraReji@users.noreply.github.com>
2023-11-13 11:37:20 +0530

    Update main.c

--- main.c
+++ main.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "project_header.h"
 #include <string.h>
+#include "project_header.h"
 
-int main()
-{
+#define MAX_INPUT_LEN 100
+
+int main() {
     int no_of_input_rows;
     struct FlightInfo data[MAX_ROWS];
     int unique_places_rows = 0;
@@ -15,15 +16,16 @@ int main()
     char start[MAX_LEN], end[MAX_LEN];
     int allpaths_row;
 
-    printf("came to main.\n");
+    printf("Welcome to the Flight and Hotel Booking System.\n");
 
     memset(data, 0, sizeof(data));
     memset(adj, 0, sizeof(adj));
 
     no_of_input_rows = get_flight_data(data, MAX_ROWS);
 
-    if(no_of_input_rows == -1) {
-        return no_of_input_rows;
+    if (no_of_input_rows == -1) {
+        printf("Error reading flight data.\n");
+        return -1;
     }
 
     unique_places_rows = find_all_unique_places(data, places, no_of_input_rows);
@@ -35,54 +37,61 @@ int main()
     display_adj(adj, unique_places_rows);
 
     printf("Enter the source: ");
-    gets(start);
-
-    //strcpy(start, "Natal (RN)");
+    fgets(start, sizeof(start), stdin);
+    start[strcspn(start, "\n")] = '\0';  // Remove the newline character
 
     printf("Enter the destination: ");
-    gets(end);
-
-    //strcpy(end, "Salvador (BH)");
-
-    printf("got start and end.\n");
+    fgets(end, sizeof(end), stdin);
+    end[strcspn(end, "\n")] = '\0';  // Remove the newline character
 
     allpaths_row = find_path(start, end, unique_places_rows, &all_paths_src_dst, adj, places);
-    if(allpaths_row == -1) {
-        printf("Bad input.\n");
+    if (allpaths_row == -1) {
+        printf("Error finding paths.\n");
         return -1;
     }
 
     find_total_cost_duration(all_paths_src_dst, allpaths_row, adj);
 
-    /*heapSortByDuration(all_paths_src_dst, allpaths_row);
-
-    printf("\nSorted flights by duration:\n");
-    for (int i = 0; i < allpaths_row; i++) {
-        printf("Duration: %d\n", all_paths_src_dst[i].duration);
+    int sort_type;
+    printf("Enter the sort type (1 for duration, 2 for cost): ");
+    if (scanf("%d", &sort_type) != 1) {
+        printf("Invalid input for sort type.\n");
+        return -1;
     }
 
-    printf("done with sorting.\n");*/
-    int sort_type;
-  printf("Enter the sort type (1 for duration, 2 for cost): ");
-  scanf("%d", &sort_type);
+    switch (sort_type) {
+        case 1:
+            heapSortByDuration(all_paths_src_dst, allpaths_row, places);
+            break;
+        case 2:
+            heapSortByCost(all_paths_src_dst, allpaths_row, places);
+            break;
+        default:
+            printf("Invalid sort type.\n");
+            return -1;
+    }
 
-  switch (sort_type) {
-    case 1:
-      heapSortByDuration(all_paths_src_dst, allpaths_row,places);
-      break;
-    case 2:
-      heapSortByCost(all_paths_src_dst, allpaths_row,places);
-      break;
-    default:
-      printf("Invalid sort type.\n");
-      return -1;
-  }
- /* if (allpaths_row > 0) {
-    struct allpaths first_element = all_paths_src_dst[0];
-    printf("First element after sorting:\n");
-    // Access properties of first_element like first_element.duration, first_element.cost, etc.
-} else {
-    printf("No paths found.\n");
-}*/
+    struct HotelInfo hotels[MAX_HOTELS];
+    int hotelCount = readHotelsData(hotels, "hotels.csv");
+
+    printf("Hotels:\n");
+    for (int i = 0; i < hotelCount; i++) {
+        printf("Name: %s, Place: %s, Price: %.2f\n", hotels[i].name, hotels[i].place, hotels[i].price);
+    }
+
+    struct HotelInfo *cheapestHotel = findCheapestHotel(end);
+
+    if (cheapestHotel != NULL) {
+        printf("Cheapest hotel in %s:\n", end);
+        printf("Name: %s, Price: %.2f\n", cheapestHotel->name, cheapestHotel->price);
+    } else {
+        printf("No hotels found in %s.\n", end);
+    }
+
+    // Clean up dynamically allocated memory if any
+    // (e.g., all_paths_src_dst, and any other dynamically allocated data)
+
+
+    return 0;
 }
 
