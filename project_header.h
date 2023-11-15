6d9123a629ca48dc091a0fa4935349563806d441
not-meg <116863014+not-meg@users.noreply.github.com>
2023-11-15 15:17:55 +0530

    Update project_header.h

--- project_header.h
+++ project_header.h
@@ -1,5 +1,5 @@
-#ifndef PROJECT_HEADER_H_INCLUDED
-#define PROJECT_HEADER_H_INCLUDED
+//#ifndef PROJECT_HEADER_H_INCLUDED
+//#define PROJECT_HEADER_H_INCLUDED
 #define MAX_ROWS 200
 #define MAX_COLS 6
 #define MAX_LEN 30
@@ -45,22 +45,36 @@ struct allpaths
 };
 
 void display_adj(struct adjacency_matrix_data adj[MAX_ROWS][MAX_ROWS], int rows);
+
 void display_places(char places[MAX_ROWS][MAX_LEN], int rows);
+
 int get_flight_data(struct FlightInfo *data, int maxFlightCount);
+
 int find_all_unique_places(struct FlightInfo *data, char places[MAX_ROWS][MAX_LEN], int no_of_rows);
+
 void create_adjacency_matrix(struct FlightInfo *data, struct adjacency_matrix_data adj[MAX_ROWS][MAX_ROWS], int no_of_input_rows,
                              char places[MAX_ROWS][MAX_LEN], int unique_places_rows);
+
 int find_path(char start[MAX_LEN], char end[MAX_LEN], int unique_places_rows,
-              struct allpaths **all_paths_src_dst, struct adjacency_matrix_data adj[MAX_ROWS][MAX_ROWS],
-              char places[MAX_ROWS][MAX_LEN]);
+             struct allpaths **all_paths_src_dst,
+             struct adjacency_matrix_data adj[MAX_ROWS][MAX_ROWS],
+             char places[MAX_ROWS][MAX_LEN]);
+
 void find_total_cost_duration(struct allpaths *all_paths_src_dst, int allpaths_row,
                               struct adjacency_matrix_data adj[MAX_ROWS][MAX_ROWS]);
+
 void heapSortByDuration(struct allpaths arr[], int allpaths_row,char places[MAX_ROWS][MAX_LEN]);
+
+void heapSortByCost(struct allpaths arr[], int n,char places[MAX_ROWS][MAX_LEN]);
+
 int readHotelsData(struct HotelInfo hotels[], const char *filename);
+
 int hashDestination(const char* destination);
+
 void insertHotel(const struct HotelInfo* hotel);
+
 struct HotelInfo* findCheapestHotel(const char* destination);
 
 
 
-#endif // PROJECT_HEADER_H_INCLUDED
+//#endif // PROJECT_HEADER_H_INCLUDED
